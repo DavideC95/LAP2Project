957f2663ad22f76c15f2e1e5156023c4d8e34624
Davide Costa <davide_pucca@hotmail.it>
2017-06-28 17:07:55 +0200

    Authentication basis

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import firebase from 'firebase';
+
+const initialState={};
+
+class App extends React.Component{
+  componentWillMount(){
+    var config = {
+      apiKey: "AIzaSyBHMEIacNLH96rpl9P9Xa8WSBFkUYPbC5Y",
+      authDomain: "lap2project-a3f29.firebaseapp.com",
+      databaseURL: "https://lap2project-a3f29.firebaseio.com",
+      projectId: "lap2project-a3f29",
+      storageBucket: "lap2project-a3f29.appspot.com",
+      messagingSenderId: "244675529880"
+    };
+    firebase.initializeApp(config);
+  }
+}
+
 export default class App extends React.Component {
   render() {
     return (
---
+++ actions/authActions.js
@@ -0,0 +1,56 @@
+import {
+  LOGIN_USER_START,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL,
+  REGISTER_USER_START,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL
+} from './types';
+
+import firebase from 'firebase';
+
+export const loginUser = ({ email, password, navigateTo }) => {
+  return (dispatch) => {
+    console.log("LoginUser action: ", email, password);
+    dispatch({ type: LOGIN_USER_START });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user, navigateTo))
+      .catch(error => loginUserFailed(dispatch, error));
+  }
+};
+
+export const registerUser = ({ email, password, navigateTo }) => {
+	return(dispatch) => {
+		console.log("RegisterUser action: ", email, password);
+		dispatch({type: REGISTER_USER_START });
+		firebase.auth().createUserWithEmailAndPassword(email, password)
+			.then(user => registerUserSuccess(dispatch, user, navigateTo)) //Da implementare la registerSuccess
+			.catch(error => registerUserFailed(dispatch, error));
+	}
+};
+
+//Funzioni di gestione Login
+const loginUserSuccess = (dispatch, user, navigateTo) => {
+  console.log("utente loggato", user);
+  dispatch({ type: LOGIN_USER_SUCCESS, payload: user })
+  // vai a home screen
+  navigateTo('home')
+}
+
+const loginUserFailed = (dispatch, error)  => {
+  console.log("Impossibile effettuare il Login");
+  dispatch({ type: LOGIN_USER_FAIL, payload: error })
+}
+
+//Funzioni di gestione Register
+const registerUserSuccess = (dispatch, user, navigateTo) => {
+  console.log("utente loggato", user);
+  dispatch({ type: REGISTER_USER_SUCCESS, payload: user })
+  //Reindirizzamento a Login Screen
+  navigateTo('Login')
+}
+
+const loginUserFailed = (dispatch, error)  => {
+  console.log("non sono riuscito neanche a creare un account");
+  dispatch({ type: LOGIN_USER_FAIL, payload: error })
+}
